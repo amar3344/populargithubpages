76afa48bca810580fdd310f2e3dd264fefd7039c
amarkumar <amarkumarreddynew@gmail.com>
2023-02-17 13:01:54 +0000

    update on responsive

--- src/components/GithubPopularRepos/index.js
+++ src/components/GithubPopularRepos/index.js
@@ -26,14 +26,13 @@ const GithubPopularRepos = () => {
   useEffect(() => {
     const getGithubRepos = async () => {
       const url = `https://apis.ccbp.in/popular-repos?language=${activeTabId}`
-      const options = {
-        method: 'GET',
-      }
-      const res = await fetch(url, options)
+
+      const res = await fetch(url)
       const data = await res.json()
-      if (res.ok === 200) {
+      if (res.ok === true) {
         setGitRepoList(data.popular_repos)
         setApiStatus(apiStatusInState.success)
+        console.log(data.popular_repos)
       } else {
         setApiStatus(apiStatusInState.failure)
       }
@@ -41,42 +40,36 @@ const GithubPopularRepos = () => {
     getGithubRepos()
   }, [])
 
-  const getStatusApi=()=>
-
-    (
-         switch (apiStatus){
-                case (apiStatusInState.inProgress):
-                    return getApiInProgress()
-                case (apiStatusInState.inProgress):
-                    return getApiInProgress()
-                case (apiStatusInState.inProgress):
-                    return getApiInProgress()
-                default :
-                return null
-            }
-
-    )
-
-  
+  const getGithubRepos = () => (
+    <ul className="gitrepo-list-container">
+      {getGitRepoList.map(item => (
+        <RepositoryItem key={item.id} details={item} />
+      ))}
+    </ul>
+  )
 
   return (
     <div className="react-app">
       <div className="github-app-container">
-        <h3>Popular</h3>
+        <h3 className="heading">Popular</h3>
         <div>
-          <button type="button">All</button>
-          <button type="button">Javascript</button>
-          <button type="button">Ruby</button>
-          <button type="button">Java</button>
-          <button type="button">CSS</button>
+          <button type="button" className="tab-buttons">
+            All
+          </button>
+          <button type="button" className="tab-buttons">
+            Javascript
+          </button>
+          <button type="button" className="tab-buttons">
+            Ruby
+          </button>
+          <button type="button" className="tab-buttons">
+            Java
+          </button>
+          <button type="button" className="tab-buttons">
+            CSS
+          </button>
         </div>
-
-        {getStatusApi()}
-        {/* <ul>
-          {getGitRepoList.map(item => (
-            <RepositoryItem key={item.id} details={item} />
-          ))}
-        </ul> */}
+        {getGithubRepos()}
       </div>
     </div>
   )
--- src/components/RepositoryItem/index.js
+++ src/components/RepositoryItem/index.js
@@ -1,9 +1,40 @@
+import './index.css'
+
 const RepositoryItem = props => {
   const {details} = props
   return (
-    <div>
-      <h2>{details.name}</h2>
-    </div>
+    <li className="each-list-item">
+      <button type="button">
+        <img
+          src={details.avatar_url}
+          alt={details.name}
+          width={100}
+          height={100}
+        />
+        <p className="name-text">{details.name}</p>
+        <div className="stars-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+            alt="stars"
+          />
+          <p>{details.stars_count} stars</p>
+        </div>
+        <div className="stars-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+            alt="forks"
+          />
+          <p>{details.forks_count} forks</p>
+        </div>
+        <div className="stars-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png"
+            alt="open issues"
+          />
+          <p>{details.issues_count} open issues</p>
+        </div>
+      </button>
+    </li>
   )
 }
 
